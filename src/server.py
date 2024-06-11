5ac1fc40470b44e0974d0565047b1b84227028cb
Samarth Nasnodkar <samarthnasnodkar@gmail.com>
2024-06-11 13:50:11 +0530

    created a venv to make life easy and improved UI

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from utils.news import News
 
 app = Flask(__name__)
+news = News()
 
 @app.route('/', methods=['GET'])
 def home():
@@ -11,13 +12,17 @@ def home():
 @app.route('/details', methods=['GET'])
 def details():
   url = request.args.get('url')
-  articleContent = News.fetchNewsContent(url)
+  articleContent = news.fetchNewsContent(url)
   return render_template('details.html', content=articleContent)
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
   articleText = request.form['article-text']
-  return '<p>' + articleText + '</p>'
+  prediction = news.predict(articleText)
+  if prediction == 0:
+    return '<p>Fake News</p>'
+  
+  return '<p>Real News</p>'
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=8000, debug=True)
--- src/utils/news.py
+++ src/utils/news.py
@@ -1,12 +1,47 @@
 import newspaper
+import pickle
+import numpy as np
+import pandas as pd
+import re
+import string
+
+def processText(text: str) -> str:
+  text = text.lower()
+  text = re.sub('\[.*?\]', '', text)
+  text = re.sub("\\W"," ",text) 
+  text = re.sub('https?://\S+|www\.\S+', '', text)
+  text = re.sub('<.*?>+', '', text)
+  text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+  text = re.sub('\n', '', text)
+  text = re.sub('\w*\d\w*', '', text)    
+  return text
 
 class News:
   def __init__(self) -> None:
-    pass
+    self.lRModel = pickle.load(open('../ml/models/lr.pkl', 'rb'))
+    self.DTModel = pickle.load(open('../ml/models/dt.pkl', 'rb'))
+    self.RFCModel = pickle.load(open('../ml/models/rfc.pkl', 'rb'))
+    self.GBCModel = pickle.load(open('../ml/models/gbc.pkl', 'rb'))
+    self.vectorization = pickle.load(open('../ml/models/vectorizer.pkl', 'rb'))
 
-  @staticmethod
-  def fetchNewsContent(url: str) -> str:
+  def fetchNewsContent(self, url: str) -> str:
     article = newspaper.Article(url)
     article.download()
     article.parse()
     return article.text
+  
+  def predict(self, articleText: str) -> str:
+    testing_news = {"text": [articleText]}
+    df_test = pd.DataFrame(testing_news)
+    df_test['text'] = df_test['text'].apply(processText)
+    x_test = df_test['text']
+    xv_test = self.vectorization.transform(x_test)
+    preds = [
+      self.lRModel.predict(xv_test)[0], 
+      self.DTModel.predict(xv_test)[0], 
+      self.RFCModel.predict(xv_test)[0], 
+      self.GBCModel.predict(xv_test)[0]
+      ]
+    print(f'{preds =}')
+    return preds[2]
+    # return np.bincount(preds).argmax()
